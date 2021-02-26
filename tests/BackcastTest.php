bac77fccb358057802f0caba767adcef0b58152a
Tom McFarlin <tom@tommcfarlin.com>
2021-02-26 18:01:33 -0500

    Updated tests to use the sample.opml

--- tests/BackcastTest.php
+++ tests/BackcastTest.php
@@ -21,40 +21,40 @@ class BackcastTest extends TestCase
 
 	public function testFileExists(): void
 	{
-		$path = __DIR__ . '/exports/overcast.opml';
+		$path = __DIR__ . '/exports/sample.opml';
 		$backcast = new Backcast($path);
 		$this->assertTrue($backcast->exportFileExists());
 	}
 
 	public function testIsValidFileType(): void
 	{
-		$path = __DIR__ . '/exports/overcast.opml';
+		$path = __DIR__ . '/exports/sample.opml';
 		$backcast = new Backcast($path);
 		$this->assertTrue($backcast->hasValidFileType());
 	}
 
 	public function testContainsOpmlTag() : void
 	{
-		$path = __DIR__ . '/exports/overcast.opml';
+		$path = __DIR__ . '/exports/sample.opml';
 		$backcast = new Backcast($path);
 		$this->assertTrue($backcast->containsOpmlTag());
 	}
 
 	public function testIsValidOpml() : void
 	{
-		$path = __DIR__ . '/exports/overcast.opml';
+		$path = __DIR__ . '/exports/sample.opml';
 		$backcast = new Backcast($path);
 		$this->assertTrue($backcast->isValidOpml());
 	}
 
 	public function testHasProperOutlineTags() {
-		$path = __DIR__ . '/exports/overcast.opml';
+		$path = __DIR__ . '/exports/sample.opml';
 		$backcast = new Backcast($path);
 		$this->assertTrue($backcast->hasProperOutlineTags());
 	}
 
 	public function testHasProperXmlUrls() {
-		$path = __DIR__ . '/exports/overcast.opml';
+		$path = __DIR__ . '/exports/sample.opml';
 		$backcast = new Backcast($path);
 		$this->assertTrue($backcast->hasProperXmlUrls());
 	}
